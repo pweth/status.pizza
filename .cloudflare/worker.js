a8fb0cba28537220af6f4e2dcf9176496406bab4
Peter Wetherall <22416843+pweth@users.noreply.github.com>
2022-05-17 16:46:13 +0100

    Cloudflare Worker script
    Clone of the backend service script for the site.

---
+++ .cloudflare/worker.js
@@ -0,0 +1,103 @@
+addEventListener("fetch", (event) => {
+    event.respondWith(
+        handleRequest(event.request).catch(
+        (err) => new Response(err.stack, { status: 500 })
+        )
+    );
+});
+
+let codes = {
+    "100": "Continue",
+    "101": "Switching Protocols",
+    "102": "Processing",
+    "200": "OK",
+    "201": "Created",
+    "202": "Accepted",
+    "203": "Non-Authoritative",
+    "204": "No Content",
+    "206": "Partial Content",
+    "207": "Multi-Status",
+    "300": "Multiple Choices",
+    "301": "Moved Permanently",
+    "302": "Found",
+    "303": "See Other",
+    "304": "Not Modified",
+    "305": "Use Proxy",
+    "307": "Temporary Redirect",
+    "308": "Permanent Redirect",
+    "400": "Bad Request",
+    "401": "Unauthorized",
+    "402": "Payment Required",
+    "403": "Forbidden",
+    "404": "Not Found",
+    /*...*/
+    "422": "Unprocessable Entity",
+    /*...*/
+    "500": "Internal Server Error",
+    /*...*/
+    "521": "Web Server Is Down",
+};
+  
+async function handleRequest(request) {
+    const { pathname } = new URL(request.url);
+
+    // codes.js request
+    if (pathname == "/codes.js") {
+        return new Response("let codes=" + JSON.stringify(codes) + ";", {
+            headers: {
+                "content-type": "text/javascript",
+            },
+        });
+    }
+
+    // No extension or .png request
+    if (pathname.length == 4 || (pathname.length == 8 && pathname.substring(4, 8).toLowerCase() == ".png")) {
+        let code = pathname.substring(1, 4);
+        if (Object.keys(codes).indexOf(code) > -1) {
+            return fetch("https://pweth.github.io/status.pizza/srv/" + code + ".png");
+        }
+    }
+
+    // .json request
+    if (pathname.length == 9 && pathname.substring(4, 9).toLowerCase() == ".json") {
+        let code = pathname.substring(1, 4);
+        if (Object.keys(codes).indexOf(code) > -1) {
+            let cat, integer = parseInt(code);
+            if (integer < 200) {
+                cat = "Informational";
+            } else if (integer < 300) {
+                cat = "Successful";
+            } else if (integer < 400) {
+                cat = "Redirection";
+            } else if (integer < 500) {
+                cat = "Client error";
+            } else {
+                cat = "Server error";
+            }
+            return new Response(JSON.stringify({
+                "code": code,
+                "title": codes[code],
+                "image": "https://status.pizza/" + code,
+                "raw": "https://status.pizza/img/" + code + ".png",
+                "category": cat
+            }), {
+                headers: {
+                    "Content-Type": "application/json" 
+                },
+            });
+        }
+    }
+
+    // favicon.ico request
+    if (pathname == "/favicon.ico") {
+        return fetch("https://pweth.github.io/status.pizza/favicon.png");
+    }
+
+    // Attempt to return static asset from GitHub
+    let static = await fetch("https://pweth.github.io/status.pizza" + pathname);
+    if (static.status == 200) {
+        return static;
+    } else {
+        return fetch("https://pweth.github.io/status.pizza/srv/404.png");
+    }
+}
