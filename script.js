3c782d4f9a9a3640aed36622e1a6068185152289
Peter Wetherall <22416843+pweth@users.noreply.github.com>
2022-05-17 16:41:54 +0100

    Initial file upload 🎉

---
+++ script.js
@@ -0,0 +1,34 @@
+let files = [];
+for (let code of Object.keys(codes)) {
+    let anchor = document.createElement("a");
+    anchor.href = "https://status.pizza/" + code;
+    anchor.setAttribute("target", "_blank");
+    let content = document.createElement("content");
+    let image = document.createElement("img");
+    image.src = "img/" + code + ".png";
+    image.draggable = false;
+    content.appendChild(image);
+    let disp = document.createElement("span");
+    disp.innerHTML = code;
+    content.appendChild(disp);
+    let desc = document.createElement("span");
+    desc.innerHTML = codes[code];
+    content.appendChild(desc);
+    anchor.appendChild(content);
+    document.getElementById("ship").appendChild(anchor);
+    files.push(code);
+}
+new Typed("#code", {
+    strings: files,
+    shuffle: true,
+    typeSpeed: 100,
+    backSpeed: 50,
+    smartBackspace: false,
+    loop: true,
+    backDelay: 2500
+});
+document.getElementById("scroll").addEventListener("click", () => {
+    document.getElementById("pizza").scrollIntoView({ 
+        behavior: "smooth" 
+    });
+});
